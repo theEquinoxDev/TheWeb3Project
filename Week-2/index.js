d38030b948df54829367cece4d1b6712cd7dfa6a
Aditya <adityasuri11ss@gmail.com>
2025-09-18 23:50:57 +0530

    Added some notes

--- Week-2/index.js
+++ Week-2/index.js
@@ -18,16 +18,20 @@
 // let bytes = new Uint8Array([0, 255, 127, 128]);
 // console.log(bytes);
 
-// Why use UInt8Array over native Arrays ? 
+// Why use UInt8Array over native Arrays ?
 // 1. They use less space. Every number takes 64 bits (8 bytes). But every value in a UInt8Array takes 1 byte.
 // 2. UInt8Array Enforces constraints - It makes sure every element doesn’t exceed 255
 
-// Verify: 
-// What do you think happens to the first element here? Does it throw an error? 
+// Verify:
+// What do you think happens to the first element here? Does it throw an error?
 
-let uint8Arr = new Uint8Array([0, 255, 127, 128]);
-uint8Arr[1] = 300;
-console.log(uint8Arr); // It makes sure that any number in the array doesn't exceed the limit of 255. 
+// let uint8Arr = new Uint8Array([0, 255, 127, 128]);
+// uint8Arr[1] = 300;
+// console.log(uint8Arr); // It makes sure that any number in the array doesn't exceed the limit of 255.
 
+// let str = "h";
+// const binaryRepresentation = new TextEncoder().encode(str); // TextEncoder is a class which encodes some text ("h" here) into binary! 
+// console.log(binaryRepresentation);
 
+// maximum value of a byte = 255;
 
