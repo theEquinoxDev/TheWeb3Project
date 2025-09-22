6f19d119ac69c5d83eec2a77c4b0de981fa5bc76
Aditya <adityasuri11ss@gmail.com>
2025-09-22 22:05:37 +0530

    notes added

--- Week-3/index.js
+++ Week-3/index.js
@@ -9,3 +9,4 @@
 // . The public key is then hashed using the Keccak-256 algorithm. 
 // . After hashing the public key with Keccak-256, you got a 32-byte hash. The Ethereum address is derived from this hash by taking only the last 20 bytes of the hash output. 
 // . The resulting 20 byte value is then converted into hex format and prefixed with '0x' to form the Ethereum address. 
+
