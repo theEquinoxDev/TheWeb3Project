c0e92df26c842dfeec76473ac77ed2e60f64e7bb
Aditya <adityasuri11ss@gmail.com>
2025-09-23 23:05:13 +0530

    more notes added

--- Week-3/index.js
+++ Week-3/index.js
@@ -21,3 +21,18 @@
 
 // 1. To send a `transction`
 // 2. To fetch some details from the blockchain (balances etc)
+
+// Wei:
+
+// - Definition: Wei is the smallest unit of cryptocurrency in the Ethereum network. It's similar to how a cent is to a dollar.
+// - Value: 1 Ether (ETH) = 10^18 Wei.
+
+//Gwei
+
+// - Definition: A larger unit of Ether commonly used in the context of gas prices.
+// - Conversion: 1 Ether = 10^9 gwei
+
+//  Lamports
+
+// - In the Solana blockchain, the smallest unit of currency is called a lamport. Just as wei is to Ether in Ethereum, lamports are to SOL (the native token of Solana). 
+// 1 SOL = 10 ^ 9 Lamports
