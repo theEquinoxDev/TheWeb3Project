97b78275f58c11e3b8a6ae956579cc571f81c914
Aditya <adityasuri11ss@gmail.com>
2025-09-25 23:27:36 +0530

    New Week notes

---
+++ Week-4/index.js
@@ -0,0 +1,6 @@
+// Solana Jargon, Programming model, Tokens
+
+// Eth/Sol solves for more than just currencies. 
+// They let us swap native currencies. 
+// They let the users program anything. 
+// They also let us create tokens. 
