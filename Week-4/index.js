e8972eea855b243d5447d11784e426b2ec64e6b8
Aditya <adityasuri11ss@gmail.com>
2025-09-25 23:29:27 +0530

    Notes added

--- Week-4/index.js
+++ Week-4/index.js
@@ -4,3 +4,11 @@
 // They let us swap native currencies. 
 // They let the users program anything. 
 // They also let us create tokens. 
+
+
+// Programs/Smart contracts
+
+// ETH was one of the first blockchains to introduce the concept of decentralized state / programs. These are popularly known as smart contracts on the ETH blockchain.
+// HTTP Servers are deployed on cloud providers like `GCP, Azure`
+// Smart contracts/programs are deployed on the `blockchain`
+
