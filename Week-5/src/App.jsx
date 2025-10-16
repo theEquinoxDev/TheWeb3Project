0e80748bfc9adc5cb0a98e01f57194086f07c73b
Aditya <adityasuri11ss@gmail.com>
2025-10-16 14:59:26 +0530

    implementation and installation

--- Week-5/src/App.jsx
+++ Week-5/src/App.jsx
@@ -12,16 +12,28 @@ import { Airdrop } from "./components/Airdrop";
 import { ShowBalance } from "./components/ShowBalance";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
+import { SendTokens } from "./components/SendTokens";
+import { SignMessage } from "./components/SignMessage";
 
 function App() {
   return(   
   <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/OFFU06fHM9MjGOlG_xkw7"}>
     <WalletProvider wallets={[]} autoConnect>
       <WalletModalProvider>
+        <div style={{ display: "flex", justifyContent: "space-evenly", marginTop: "40px", flexDirection: "column", alignItems: "center"}}>
+          <br></br>
+
         <WalletMultiButton/>
+        <br></br>
         <WalletDisconnectButton/>
+        <br></br>
         <Airdrop />
+       
         <ShowBalance/>
+        <SendTokens/>
+        <br></br>
+        <SignMessage/>
+        </div>
       </WalletModalProvider>
     </WalletProvider>
   </ConnectionProvider>
